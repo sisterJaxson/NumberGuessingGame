dc92c8bf68fda1f8944816de79179c5003301722
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-03-21 14:06:27 -0400

    Started making while loop

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -11,24 +11,26 @@ public class Main {
         int pickedNumber;
         keyboard = new Scanner(System.in);
 
-	pickedNumber = (int)(Math.random()*100);
-	System.out.println(pickedNumber);
-	System.out.println("Can you guess the number, 1 to 100, that I am thinking of?");
-	usernumber = keyboard.nextInt();
-
-	if (usernumber<pickedNumber){
-
-	    System.out.println("My number is greater than yours");
-
-    }
+        pickedNumber = (int) (Math.random() * 100);
+        System.out.println(pickedNumber);
+        System.out.println("Can you guess the number, 1 to 100, that I am thinking of?");
         usernumber = keyboard.nextInt();
-    if (usernumber>pickedNumber){
-	    System.out.println("My number is less than yours");
-    }
-        usernumber = keyboard.nextInt();
-    if (usernumber==pickedNumber){
-	    System.out.println("You got it right!");
-    }
+
+
+        if (usernumber == pickedNumber) {
+            System.out.println("You got it right!");
+        }
+        while (usernumber < pickedNumber || usernumber > pickedNumber){
+            if (usernumber < pickedNumber) {
+                System.out.println("My number is greater than yours");
+            }
+            if (usernumber > pickedNumber) {
+            System.out.println("My number is less than yours");
+            }
+            if (usernumber == pickedNumber) {
+            System.out.println("You got it right!");
+            }
+        }
     }
 
 }
